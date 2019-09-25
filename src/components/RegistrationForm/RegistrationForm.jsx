d43e07e0bdf91201a36ce3c520f462c606918f4c
João Mello <joaomellogomes@gmail.com>
2019-09-24 21:45:31 -0300

    Started resources section

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -7,7 +7,8 @@ const InputText = ({type, labelText, icon}) =>
         type={type}
         placeholder={labelText}
         data-icon={icon}
-        className="inputFrmRegistration"/>;
+        className="inputFrmRegistration"
+        autoComplete="false"/>;
 
 class RegistrationForm extends Component {
     render() {
@@ -16,9 +17,7 @@ class RegistrationForm extends Component {
                 <h2 id="registerFormTitle">Cadastre sua imobiliária</h2>
 
                 <InputText type="text" labelText="Nome" icon=""/>
-
                 <InputText type="email" labelText="Email" icon=""/>
-                
                 <InputText type="phone" labelText="Telefone" icon=""/>
 
                 <button className="submitRegistration" type="submit">Enviar</button>
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,39 +2,61 @@ import React, { Component } from 'react';
 import './home.scss';
 import Header from 'components/Header/Header.jsx';
 import RegistrationForm from 'components/RegistrationForm/RegistrationForm.jsx';
+import economyIcon from 'assets/svg/economy.svg';
+import centralizedIcon from 'assets/svg/centralized.svg';
+
+const Resource = (props) => {
+
+    return (
+        <div className="col s12 resourceWraper">
+            <img className="resourceIcon" src={props.imgIcon} alt={props.resourceName + ' icon'}/>
+            <dt className="resourceName">{props.resourceName}</dt>
+            <dd className="resourceDescription">{props.resourceDescription}</dd>
+        </div>
+    );
+
+};
 
 class Home extends Component {
 
     render() {
+
+        const lorem = 'lorem ipsum dolor sit amet';
+
         return (
             <>
-                <section className="container" id="secSubscribe">
-                    <div className="row">
-                        <div className="col s12">
-                            <Header />
+                <div id="containerBgOwli">
+                    <section className="container" id="secSubscribe">
+                        <div className="row">
+                            <div className="col s12 colHeader">
+                                <Header />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="row" id="rowSubscribeMain">
+                        <div className="row" id="rowSubscribeMain">
 
-                        <div className="col xl7">
-                            <h1 id="titleHomePage">A nova área do cliente</h1>
-                            <h3 id="subtitleHomePage">Aplicativo para locatários e proprietários de imóveis</h3 >
-                            <button id="btnBePart">Quero fazer parte</button>
+                            <div className="col xl7">
+                                <h1 id="titleHomePage">A nova área do cliente</h1>
+                                <h3 id="subtitleHomePage">Aplicativo para locatários e proprietários de imóveis</h3 >
+                                <button id="btnBePart">Quero fazer parte</button>
+                            </div>
+                            <div className="col xl5">
+                                <RegistrationForm />
+                            </div>
                         </div>
-                        <div className="col xl5">
-                            <RegistrationForm />
-                        </div>
-                    </div>
-                </section>
-
+                    </section>
+                </div>
                 <section className="container" id="meetOurResources">
-                    <div id="row" className="rowMeetOurResources">
-                        <div className="col xl6">
-                            <h2 id="meetResourcesTitle">Conheça todos os nossos recursos</h2>
-                        </div>
-                        <div className="col xl3"></div>
-                        <div className="col xl3"></div>
+                    <div id="rowMeetOurResources" className="row">
+                        <h2 id="meetResourcesTitle">
+                            <div>Conheça todos</div>
+                            <div>os nossos recursos</div>
+                        </h2>
+
+                        <dl id="resourceList">
+                            <Resource imgIcon={economyIcon} resourceDescription={lorem} resourceName='Economia' />
+                            <Resource imgIcon={centralizedIcon} resourceDescription={lorem} resourceName='Centralizado' />
+                        </dl>
                     </div>
                 </section>
             </>
