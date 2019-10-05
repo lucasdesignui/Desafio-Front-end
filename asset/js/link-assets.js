707ae2e65f7fca9137894ff61e59dbac165438ed
Cristiano Andrade <cristianoandrade89@hotmail.com>
2019-10-04 23:01:26 -0300

    Projeto Finalizado

--- asset/js/link-assets.js
+++ asset/js/link-assets.js
@@ -1,7 +1,3 @@
 $('head').append('<link rel="stylesheet" href="asset/css/main.css">',
 '<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=swap" rel="stylesheet">',
 '<link rel="stylesheet" href="asset/css/bootstrap.min.css">');
-
-$(document).ready(function(){
-    $('.telefone').mask('(00) 0000-0000');
-  });
